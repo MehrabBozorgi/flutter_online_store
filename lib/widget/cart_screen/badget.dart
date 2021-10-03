bf580dcd0f5ee498b81a6a0bd52027d442bbe944
Mehrab Bozorgi <mehrab.bozorgi@gmail.com>
2021-10-03 07:27:48 +0330

    flutter_online_store

---
+++ lib/widget/cart_screen/badget.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+class Badge extends StatelessWidget {
+
+  final Widget child;
+  final String value;
+  final Color color;
+
+  Badge({
+    required this.child,
+    required this.value,
+    required this.color,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      alignment: Alignment.center,
+      children: [
+        child,
+        Positioned(
+          right: 8,
+          top: 8,
+          child: Container(
+            padding: EdgeInsets.all(2.0),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10.0),
+              color: color != null ? color : Theme.of(context).primaryColor,
+            ),
+            constraints: BoxConstraints(
+              minWidth: 16,
+              minHeight: 16,
+            ),
+
+            child: Text(
+              value,
+             // value== 0 ? value : '',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 12,
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
